3d1f3d7da2fb964fc7c1c1a8fbe6e7167d401c0f
nkstamilan <37336232+nkstamilan@users.noreply.github.com>
2018-05-01 11:45:21 +0530

    Create selection in a table.py

---
+++ beginner/selection in a table.py
@@ -0,0 +1,18 @@
+create database class;                                    
+create table student(student_name varchar(100) not null,  
+          roll_no varchar(100) not null,                  
+          age int null,
+          gender varchar(100) not null,
+          section varchar(100) not null,
+          department varchar(100) not null,
+          attendance varchar(100) not null,
+          arrears varchar(100)  null,
+          position varchar(100)  null,
+          place varchar(100) not null);
+insert into student values('vijay','15bee216','21','M','B','EEE','80%','0','5','coimbatore');
+insert into student values('naveen','15bee220','21','M','B','EEE','83%','0','5','kovai');
+insert into student values('dinesh','15bee217','22','M','B','EEE','80%','0','6','thiruchengodu');
+select student_name,age from student where arrears='0' and place='coimbatore';
+
+output
+vijay	21
