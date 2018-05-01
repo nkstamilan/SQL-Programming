ed27cf29b06a156412781835c51cf00bef3f6924
nkstamilan <37336232+nkstamilan@users.noreply.github.com>
2018-05-01 12:26:58 +0530

    Update selection in a table.sql

--- beginner/selection in a table.sql
+++ beginner/selection in a table.sql
@@ -9,10 +9,12 @@ create table student(student_name varchar(100) not null,
           arrears varchar(100)  null,
           position varchar(100)  null,
           place varchar(100) not null);
-insert into student values('vijay','15bee216','21','M','B','EEE','80%','0','5','coimbatore');
-insert into student values('naveen','15bee220','21','M','B','EEE','83%','0','5','kovai');
-insert into student values('dinesh','15bee217','22','M','B','EEE','80%','0','6','thiruchengodu');
-select student_name,age from student where arrears='0' and place='coimbatore';
+insert into student values('vijay','15bee216','21','M','B','EEE','80%',null,null,'coimbatore');
+insert into student values('naveen','15bee220','21','M','B','EEE','83%',null,null,'kovai');
+insert into student values('dinesh','15bee217','22','M','B','EEE','80%',null,null,'thiruchengodu');
+select *from student;
 
 output
-vijay	21
+vijay	15bee216	21	M	B	EEE	80%	NULL	NULL	coimbatore
+naveen	15bee220	21	M	B	EEE	83%	NULL	NULL	kovai
+dinesh	15bee217	22	M	B	EEE	80%	NULL	NULL	thiruchengodu
